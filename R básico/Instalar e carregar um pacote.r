8a1fbeeeb749fcd91d8262986dcfb613d92513fd
Jose Guilherme Lopes <lopes.jguilherme@gmail.com>
2018-08-22 13:46:23 -0300

    .

---
+++ R básico/Instalar e carregar um pacote.r
@@ -0,0 +1,18 @@
+# Para instalar um pacote no R, há três caminhos:
+
+  # 1. Clique em 'Tools' e em seguida 'Install Packages'. Digite o nome do pacote e instale.
+  # 2. No painel 'Packages' (ao lado do Help), clique em 'Install'.
+  # 3. O caminho mais prático é executando o comando 'install.packages("nomedopacote")'. Ex:
+
+install.packages("tidyverse") # Este comando instala o pacote 'tidyverse'.
+
+
+# Para carregar um pacote no R, você pode:
+
+  # No painel 'Packages', selecionar o pacote desejado.
+  # Com o comando 'library(nomedopacote)' ou 'require(nomedopacote)". Ex:
+
+library(tidyverse) # Este comando carrega o pacote 'tidyverse'.
+
+
+# Lembre-se que você somente poderá usar as funções de um pacote após carregá-lo.
