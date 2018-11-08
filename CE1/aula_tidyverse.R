5192c055b7ea88d9213a3002b804ff2cafed3669
Jose Guilherme Lopes <lopes.jguilherme@gmail.com>
2018-11-08 16:10:32 -0200

    .

---
+++ CE1/aula_tidyverse.R
@@ -0,0 +1,12 @@
+## Computação em Estatística 1 - 2/2018
+
+## Aula: Introdução ao Tidyverse
+
+
+
+# Instalando e carregando o pacote ----------------------------------------
+
+install.packages("tidyverse")
+
+library(tidyverse)
+
